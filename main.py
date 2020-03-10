b5bc44d6db6f871159b2b525539bb44031d3aa5f
Asen M <dabicata@gmail.com>
2020-03-10 17:56:03 +0200

    creating multiple files

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import json
 mylist=[]
 with open('/home/toorhax/PycharmProjects/valery-json-parser/jsons/test.json') as f:
-    dest = open('./parsed/test.json', 'a')
     data = json.load(f)
     for x in data:
+        dest = open('./parsed/' + x, 'a')
         mydict = {"id": {"S": x['id']},
-                       "ip": {"S": x['ip']},
-                       "name": {"S": x['name']}
-                       }
+                  "ip": {"S": x['ip']},
+                  "name": {"S": x['name']}
+                  }
         mylist.append(mydict)
-    json.dump(mylist, dest)
+        json.dump(mylist, dest)
